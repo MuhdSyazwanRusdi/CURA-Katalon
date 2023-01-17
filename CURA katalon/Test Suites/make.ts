482c00a132b62970c748c4bc2fc214aebf3009cb
MuhdSyazwanRusdi <muhammadsyazwanrusdi@gmail.com>
2023-01-17 12:32:29 +0800

    lalala

---
+++ CURA katalon/Test Suites/make.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>make</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ad57cfed-a403-439d-9bb8-5533bf105e74</testSuiteGuid>
+   <testCaseLink>
+      <guid>55e569b1-fb05-462e-9f55-c346fe617f7b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1_make_appoinment</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
